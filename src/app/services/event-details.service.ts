d83b27d387a3daee1fcec15a944b5f766d119913
PriyashanJayasankhaCrazy <priyashanshell@gmail.com>
2019-10-10 22:41:25 +0530

    modified to give the output for selected conditions

--- src/app/services/event-details.service.ts
+++ src/app/services/event-details.service.ts
@@ -11,8 +11,29 @@ export class EventDetailsService {
 
   constructor( private firestore: AngularFirestore ) { }
 
-  getEvents() {
+  getAllEvents() {
     return this.firestore.collection('events').snapshotChanges();
+  }
+
+  getShowingEvents( status: number, club: string ) {
+
+    if ( club === 'all' && status === 99 ) {
+      return this.firestore.collection('events').snapshotChanges();
+
+    } else if ( club === 'all' && status !== 99 ) {
+        return this.firestore.collection('events', ref =>
+          ref.where( 'status', '==', status )  ).snapshotChanges();
+
+    } else if ( club !== 'all' && status === 99 ) {
+        return this.firestore.collection('events', ref =>
+          ref.where( 'club', '==', club )  ).snapshotChanges();
+
+    } else {
+      return this.firestore.collection('events', ref =>
+        ref.where( 'status', '==', status )
+           .where( 'club', '==', club )  ).snapshotChanges();
+    }
+
   }
 
 }
